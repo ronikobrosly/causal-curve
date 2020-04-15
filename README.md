2b4778f9f46c9f9db419e869bca1946d71fa737d
Roni Kobrosly <roni.kobrosly@gmail.com>
2020-04-15 14:55:54 -0500

    added basic repo files

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # causal-curve
 A python library with tools to perform causal inference using observational data when the treatment of interest is continuous.
+
+
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Support](#support)
+- [Contributing](#contributing)
+
+## Installation
+To install this package you
+
+## Usage
+
+## Support
+
+## Contributing
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="causal-curve", # Replace with your own username
+    version="0.0.1",
+    author="Roni Kobrosly",
+    author_email="roni.kobrosly@gmail.com",
+    description="A python library with tools to perform causal inference using \
+        observational data when the treatment of interest is continuous.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ronikobrosly/causal-curve",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
