04acbc4bda13e34d40016b3c9bb9524dd6dca19d
Roni Kobrosly <roni.kobrosly@gmail.com>
2020-04-20 16:57:19 -0500

    updated reqs.txt

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6.7',
 )
