a181b1e9ef853cbf043c378ba922861804ae5ea1
Roni Kobrosly <roni.kobrosly@gmail.com>
2020-04-20 21:25:20 -0500

    WIP for setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 setuptools.setup(
     name="causal-curve",
-    version="0.0.3",
+    version="0.0.5",
     author="Roni Kobrosly",
     author_email="roni.kobrosly@gmail.com",
     description="A python library with tools to perform causal inference using \
@@ -22,6 +19,21 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6.7',
-    install_requires=required
+    python_requires='>=3.6',
+    install_requires=[
+        'future',
+        'joblib',
+        'numpy',
+        'pandas',
+        'patsy',
+        'progressbar2',
+        'pygam',
+        'python-dateutil',
+        'python-utils',
+        'pytz',
+        'scikit-learn',
+        'scipy',
+        'six',
+        'statsmodels'
+    ]
 )
